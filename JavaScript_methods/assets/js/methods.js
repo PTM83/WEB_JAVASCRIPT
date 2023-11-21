708da00f2f7fd1890d75691e2cfad1088b3d5939
Pablo Torres <torres.m.pablo@gmail.com>
2023-11-21 18:24:14 -0300

    desafio completo

--- JavaScript_methods/assets/js/methods.js
+++ JavaScript_methods/assets/js/methods.js
@@ -1,17 +1,16 @@
 var taskCount = 0;
 //const TASKLIST = [];
 //const IDLIST = [];
-const COMPLETELIST = [];
+let COMPLETELIST = [];
 
 function addTask(event) {
     //Evita que el formulario se envíe y recarge la página
     event.preventDefault();
 
     // Obtener el valor del elemento <input> con el id "taskInput"
-
     var taskInput = document.getElementById("taskInput").value;
-    // Verificar que taskInput no se encuentre vacío
 
+    // Verificar que taskInput no se encuentre vacío
 
     if(taskInput) {
         //Incrementar número de tareas
@@ -23,8 +22,14 @@ function addTask(event) {
         var taskTable = document.getElementById("taskTable");
 
         //Crea una fila y la adhiere al final de la tabla
-        var row = taskTable.tBodies[0].insertRow(-1);
+        //var row = taskTable.tBodies[0].insertRow(-1);
         // Crear dos nuevas celdas y las agrega a la fila
+
+        var row = taskTable.tBodies[0].insertRow(-1);
+        //row.id = 'task-' + taskCount; // Asignar un ID único a la fila
+        row.id = 'task-' + taskCount;
+
+        //console.log(row.id);
 
         var idCell = row.insertCell(0);
         var taskCell = row.insertCell(1);
@@ -42,19 +47,18 @@ function addTask(event) {
         var checkbox = document.createElement('input');
         checkbox.type = 'checkbox';
         checkbox.checked = false; // Unchecked by default
-        checkbox.onchange = function() {
-            COMPLETELIST[taskCount - 1].completed = checkbox.checked;
-        };
+        // Use an IIFE (Immediately Invoked Function Expression) to create a closure
+        /*checkbox.onchange = (function(taskIndex) {
+            return function() {
+                console.log(taskIndex);
+                COMPLETELIST[taskIndex].completed = checkbox.checked;
+            };
+        })(taskCount - 1); // Pass the current index (taskCount - 1) to the IIFE
+        completeCell.appendChild(checkbox);*/
+
+        checkbox.dataset.taskId = taskCount; // Asignar ID de tarea al checkbox
+        checkbox.onchange = checkboxChanged;
         completeCell.appendChild(checkbox);
-
-        //Almacenar valores de Entrada
-        //TASKLIST.push(taskInput);
-        //IDLIST.push(taskCount);
-
-        console.log(COMPLETELIST);
-       /*console.log(TASKLIST);
-        console.log(IDLIST);*/
-
 
        //Limpiar la barra <input>
        document.getElementById("taskInput").value = '';
@@ -63,12 +67,75 @@ function addTask(event) {
        taskCell.setAttribute('align', 'center');
        completeCell.setAttribute('align', 'center');
 
-       // Actualizar el contenido del elemento con id="totalTask"
-        updateTaskCount();
+       updateTaskCount();
+       updateDoneTaskCount();
+
+       //console.log(COMPLETELIST);
+    };
+}
+
+function checkboxChanged() {
+    var taskId = this.dataset.taskId;
+    var task = COMPLETELIST.find(t => t.id == taskId);
+    if (task) {
+        task.completed = this.checked;
+    }
+}
+
+function addCompleteTask() {
+    var completeTable = document.getElementById("DoneTable");
+
+    // Filtrar tareas completadas y actualizar COMPLETELIST
+    var tasksToKeep = [];
+    COMPLETELIST.forEach(task => {
+        if (task.completed) {
+            // Agregar a DoneTable
+            var row_complete = completeTable.tBodies[0].insertRow(-1);
+            var idCompleteCell = row_complete.insertCell(0);
+            var taskCompleteCell = row_complete.insertCell(1);
+
+            idCompleteCell.textContent = task.id;
+            taskCompleteCell.textContent = task.description;
+
+            idCompleteCell.setAttribute('align', 'center');
+            taskCompleteCell.setAttribute('align', 'center');
+
+            removeTaskFromTable(task.id);
+        } else {
+            tasksToKeep.push(task);
+        }
+    });
+
+    //console.log(tasksToKeep)
+
+    COMPLETELIST = tasksToKeep;
+    updateTaskCount();
+    updateDoneTaskCount(); // Actualizar el contador de tareas completadas
+
+}
+
+function removeTaskFromTable(taskId) {
+    var taskTable = document.getElementById("taskTable");
+    var tbody = taskTable.tBodies[0];
+    var rows = tbody.rows;
+
+    for (let i = 0; i < rows.length; i++) {
+        var checkbox = rows[i].querySelector("input[type='checkbox']");
+        if (checkbox && checkbox.dataset.taskId == taskId) {
+            tbody.deleteRow(i);
+            break;
+        }
     }
 }
 
 function updateTaskCount() {
     const totalTaskElement = document.getElementById("totalTask");
     totalTaskElement.innerHTML = `Total: ${COMPLETELIST.length}`;
+}
+
+function updateDoneTaskCount() {
+    const doneTable = document.getElementById("DoneTable").tBodies[0];
+    const taskDoneElement = document.getElementById("TaskDone");
+    const numberOfDoneTasks = doneTable.rows.length;
+    taskDoneElement.innerHTML = `Complete: ${numberOfDoneTasks}`;
 }
